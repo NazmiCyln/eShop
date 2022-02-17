3432dbf52b17b6b1a8259f4c128a7623faf4f8a1
NazmiCyln <nazmican50@gmail.com>
2022-02-17 16:44:29 +0300

    ürünler listelendi

--- android/app/src/main/kotlin/com/example/e_ticaret/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.e_ticaret
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utilities/routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:e_ticaret/screens/product_detail.dart';
-import 'package:e_ticaret/utilities/constants.dart';
-import 'package:flutter/material.dart';
-
-class Routes{
-  static final routes = <String,WidgetBuilder>{
-    Constants.ROUTE_PRODUCT_DETAIL:(BuildContext context)=> ProductDetail()
-  };
-}
